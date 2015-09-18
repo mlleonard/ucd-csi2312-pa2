9c1c5fab72759146a0c56321207edfc29dfbd79e
mlleonard <madelineleonard@Madelines-MacBook-Air.local>
2015-09-17 19:07:04 -0600

    Initial commit

---
+++ cluster.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Madeline Leonard on 9/17/15.
+//
+
+#include "cluster.h"
---
+++ cluster.h
@@ -0,0 +1,14 @@
+//
+// Created by Madeline Leonard on 9/17/15.
+//
+
+#ifndef UCD_CSI2312_PA2_CLUSTER_H
+#define UCD_CSI2312_PA2_CLUSTER_H
+
+
+class cluster {
+
+};
+
+
+#endif //UCD_CSI2312_PA2_CLUSTER_H
